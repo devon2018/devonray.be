8c87b54c9e28fc75a249f292a61e73476d8513cd
Devon Ray <devon@devonray.net>
2024-03-27 22:09:48 +0000

    work on language

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,13 @@
-import Footer from "@/components/Shared/Footer";
-import Header from "@/components/Shared/Header";
 import type { Metadata } from "next";
 import { Georama } from "next/font/google";
 import "./globals.css";
 
-const georama = Georama({ subsets: ["latin"], variable: "--font-body" });
-
 export const metadata: Metadata = {
-  title: "Devon Ray",
-  description: "Devon Ray",
+  title: "Personal Website & Portfolio - Devon Ray",
+  description: "Personal Website & Portfolio - Devon Ray, Choose a language to get started",
   other: {
-    seobility: 'f19ca9be1a5cd340872c4f55c3234740'
-  }
+    seobility: "f19ca9be1a5cd340872c4f55c3234740",
+  },
 };
 
 export default function RootLayout({
